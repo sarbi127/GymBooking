899243af59e0586710922f7ded5e142231c39594
Sara Binazir <sarabinazir@gmail.com>
2020-01-16 10:15:36 +0100

    Add project files.

---
+++ GymBookingNC19/Data/Migrations/20200115153626_initialNeww.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GymBookingNC19.Data.Migrations
+{
+    public partial class initialNeww : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
